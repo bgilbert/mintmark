a6b0f2d2e6f268c5f2b76d40ecf2107228d99add
Benjamin Gilbert <bgilbert@backtick.net>
2023-01-14 15:35:30 -0500

    Add limit checks for custom characters

--- build.rs
+++ build.rs
@@ -14,12 +14,13 @@
  * limitations under the License.
 */
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 use std::env;
 use std::fs::{read_dir, read_to_string, write};
 use std::io::ErrorKind;
 
 const HEIGHT: usize = 9;
+const MAX_CHARS: u32 = 20;
 
 fn main() -> Result<()> {
     custom_chars()
@@ -27,6 +28,7 @@ fn main() -> Result<()> {
 
 fn custom_chars() -> Result<()> {
     let mut out: Vec<u8> = Vec::new();
+    let mut count = 0;
     for (font_name, font_num, max_width) in [("wide", 0, 12), ("narrow", 1, 10)] {
         let dir_path = format!("src/custom/{font_name}");
         println!("cargo:rerun-if-changed={}", dir_path);
@@ -43,15 +45,30 @@ fn custom_chars() -> Result<()> {
             println!("cargo:rerun-if-changed={}", ent.path().display());
 
             // read pixels from file
-            let char = ent.file_name().to_string_lossy().as_bytes()[0];
+            let filename_bytes = ent.file_name().to_string_lossy().as_bytes().to_vec();
+            if filename_bytes.len() != 1 {
+                bail!("Multi-character filename: {}", ent.path().display());
+            }
+            let char = filename_bytes[0];
+            if !(0x20..=0x7e).contains(&char) {
+                bail!("{font_name} character outside valid range: {}", char);
+            }
             let contents = read_to_string(ent.path())?;
             let pixels = contents
+                .trim_end()
                 .split('\n')
                 .map(|s| s.as_bytes())
                 .collect::<Vec<&[u8]>>();
+            if pixels.len() > HEIGHT {
+                bail!(
+                    "Character in {} too tall: {} > {HEIGHT}",
+                    ent.path().display(),
+                    pixels.len()
+                )
+            }
 
             // calculate character width
-            let w = (0..max_width)
+            let w = (0..max_width + 1)
                 .filter(|x| {
                     (0..HEIGHT).any(|y| {
                         pixels
@@ -67,6 +84,12 @@ fn custom_chars() -> Result<()> {
                 .max()
                 .map(|x| x + 1)
                 .unwrap_or(0);
+            if w > max_width {
+                bail!(
+                    "Character in {} wider than {max_width}",
+                    ent.path().display()
+                );
+            }
 
             // serialize character
             buf.extend(b"\x1b&\x02");
@@ -89,6 +112,7 @@ fn custom_chars() -> Result<()> {
                 bits <<= 16 - HEIGHT;
                 buf.extend(bits.to_be_bytes());
             }
+            count += 1;
         }
 
         // set font and store custom chars if we have any
@@ -105,6 +129,9 @@ fn custom_chars() -> Result<()> {
     }
 
     // write output
+    if count > MAX_CHARS {
+        bail!("too many custom characters: {count} > {MAX_CHARS}");
+    }
     write(
         format!("{}/custom.rs", env::var("OUT_DIR")?),
         format!("const CUSTOM_CHAR_INIT: [u8; {}] = {:?};\n", out.len(), out),
